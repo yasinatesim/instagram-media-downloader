80ccaa8e914cb3defd04f32bbab4a1e88da28940
Yasin ATEŞ <yasinatesim@gmail.com>
2022-08-22 23:46:02 +0300

    feat: add session support

--- pages/api/[username].js
+++ pages/api/[username].js
@@ -1,24 +1,55 @@
 const { IgApiClient } = require('instagram-private-api');
-const ig = new IgApiClient();
 
-async function login() {
-  ig.state.generateDevice(process.env.NEXT_PUBLIC_IG_USERNAME);
-  await ig.account.login(process.env.NEXT_PUBLIC_IG_USERNAME, process.env.NEXT_PUBLIC_IG_PASSWORD);
+const { writeFileSync, readFileSync } = require('fs');
+const path = require('path');
+
+const SESSION_FILE_PATH = path.resolve(path.join(process.cwd(), 'session.json'));
+
+function fakeSave(data) {
+  writeFileSync(SESSION_FILE_PATH, JSON.stringify(data), 'utf8');
+  return data;
 }
 
-function sleep (time) {
+function fakeExists() {
+  const data = readFileSync(SESSION_FILE_PATH, 'utf-8');
+  if (data.length > 1) return true;
+  return false;
+}
+
+function fakeLoad() {
+  return readFileSync(SESSION_FILE_PATH);
+}
+
+function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 async function Index(req, res) {
-  const { username } = req.query
+  const { username } = req.query;
 
   await sleep(2500).then(async () => {
-    await login();
+    const ig = new IgApiClient();
+    ig.state.generateDevice(process.env.NEXT_PUBLIC_IG_USERNAME);
+    await ig.qe.syncLoginExperiments();
+    ig.request.end$.subscribe(async () => {
+      const serialized = await ig.state.serialize();
+      delete serialized.constants;
+      fakeSave(serialized);
+    });
+    if (fakeExists()) {
+      try {
+        await ig.state.deserialize(fakeLoad(SESSION_FILE_PATH));
+        await ig.user.info(ig.state.cookieUserId);
+      } catch (e) {
+        console.log(e, 'SESSION NOT VALID, DOING LOGIN');
+      }
+    }
 
-    const url = (await ig.user.info(await ig.user.getIdByUsername(username))).hd_profile_pic_url_info.url;
+    await ig.account.login(process.env.NEXT_PUBLIC_IG_USERNAME, process.env.NEXT_PUBLIC_IG_PASSWORD);
+
+    const { url } = (await ig.user.info(await ig.user.getIdByUsername(username))).hd_profile_pic_url_info;
     res.status(200).json({ url });
-  })
+  });
 }
 
 export default Index;
