c01872bb07c107e799ad01b1e0d2062906c15343
Yasin ATEŞ <yasinatesim@gmail.com>
2023-03-31 01:00:50 +0300

    update path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
