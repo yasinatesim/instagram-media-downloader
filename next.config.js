5b71b8b1656345fa9fbaa6e6ffc08643bb33eb52
Yasin ATEŞ <yasinatesim@gmail.com>
2023-04-01 23:27:30 +0300

    feat: update vercel json

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
+  target: 'serverless',
 };
 
 module.exports = nextConfig;
