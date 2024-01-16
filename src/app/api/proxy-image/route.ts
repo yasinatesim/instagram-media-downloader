fc1f55295911d444ea1e8ba4ffb8dd22ec79558a
Yasin ATEŞ <yasinatesim@gmail.com>
2024-01-16 10:12:06 +0300

    fix: add api force dynamic  for mobile

--- src/app/api/proxy-image/route.ts
+++ src/app/api/proxy-image/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
 
 export async function GET (request: NextRequest, { params }: { params: { slug: string } }){
   try {
