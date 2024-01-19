97ad9f379cc9956819bd31415c975befb270c205
Yasin ATEŞ <yasinatesim@gmail.com>
2024-01-19 09:23:13 +0300

    fix: update user id service for test

--- src/app/api/get-user-info/route.ts
+++ src/app/api/get-user-info/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from 'next/server';
 
+import fetch from 'node-fetch'
+
 export const dynamic = 'force-dynamic';
 export const runtime = 'nodejs';
 export const fetchCache = 'force-no-store';
@@ -11,6 +13,7 @@ export async function GET(request: NextRequest) {
 
     const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
     const headers = {
+      'Content-Type': 'application/json',
       'User-Agent':
         'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
       'x-asbd-id': "198387",
@@ -23,13 +26,13 @@ export async function GET(request: NextRequest) {
     };
 
     const response = await fetch(url, {
-      credentials: 'include',
       headers,
     });
     const result = await response.json();
     console.log('result:', result);
 
     const data = {
+      // @ts-ignore
       userId: result.data.user.id,
     };
 
