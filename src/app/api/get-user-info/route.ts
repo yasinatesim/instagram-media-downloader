36a8e98858bc8843e299d9bfbfab7ba88b1a8060
Yasin ATEŞ <yasinatesim@gmail.com>
2024-01-19 09:15:32 +0300

    fix: update user id service for test

--- src/app/api/get-user-info/route.ts
+++ src/app/api/get-user-info/route.ts
@@ -13,18 +13,18 @@ export async function GET(request: NextRequest) {
     const headers = {
       'User-Agent':
         'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
-        "x-asbd-id": 198387,
-        "x-csrftoken": "gqeYY9dr1dCwfvtyZCgy88tcMn1A2N85",
-        "x-requested-with": "XMLHttpRequest",
-      'X-IG-App-ID': 936619743392459,
+      'x-asbd-id': "198387",
+      'x-csrftoken': 'gqeYY9dr1dCwfvtyZCgy88tcMn1A2N85',
+      'x-requested-with': 'XMLHttpRequest',
+      'X-IG-App-ID': "936619743392459",
       'sec-fetch-dest': 'empty',
       'sec-fetch-mode': 'cors',
       'sec-fetch-site': 'same-origin',
     };
 
     const response = await fetch(url, {
-      "credentials": "include",
-      headers
+      credentials: 'include',
+      headers,
     });
     const result = await response.json();
     console.log('result:', result);
