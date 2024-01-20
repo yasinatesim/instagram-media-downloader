1dc92390d223ebd1026f60cee95719433f7a5011
Yasin ATEŞ <yasinatesim@gmail.com>
2024-01-20 21:15:38 +0300

    commitlint test

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,3 @@
 export const INSTAGRAM_URL_PARAMS = '?__a=1&__d=dis'
 export const INSTAGRAM_PROFILE_URL = `https://www.instagram.com/<USER_NAME>`
-
 export const INSTAGRAM_GRAPHQL_URL = `https://www.instagram.com/graphql/query/?query_hash=de8017ee0a7c9c45ec4260733d81ea31&variables={"reel_ids":["<USER_ID>"],"tag_names":[],"location_ids":[],"highlight_reel_ids":[],"precomposed_overlay":false,"show_story_viewer_list":true,"story_viewer_fetch_count":50,"story_viewer_cursor":""}`;
