5dac98faca54b6dff99f6519f6c71134f784f860
Yasin ATEŞ <yasinatesim@gmail.com>
2024-01-27 01:47:53 +0300

    fix: update runtiime for  user info service

--- src/app/api/get-user-info/route.ts
+++ src/app/api/get-user-info/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest } from 'next/server';
 
 export const dynamic = 'force-dynamic';
-export const runtime = 'edge';
-export const fetchCache = 'force-no-store';
+// export const runtime = 'edge';
+// export const fetchCache = 'force-no-store';
 
 export async function GET(request: NextRequest) {
   try {
@@ -11,8 +11,9 @@ export async function GET(request: NextRequest) {
 
     const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
     const headers = {
-      'User-Agent': 'iphone_ua',
-      'x-ig-app-id': '936619743392459',
+      'User-Agent':
+        'Instagram 219.0.0.12.117 Android',
+      'X-IG-App-ID': `${new Date().getTime()}`,
       'sec-fetch-dest': 'empty',
       'sec-fetch-mode': 'cors',
       'sec-fetch-site': 'same-origin',
@@ -23,12 +24,9 @@ export async function GET(request: NextRequest) {
     });
 
     if (!response.ok) {
-      return Response.json(
-        {
-          error: 'error 1',
-        },
-        { status: 400 }
-      );
+      return Response.json({
+        error: 'error 1'
+      }, { status: 400 });
     }
 
     const result = await response.json();
@@ -41,12 +39,9 @@ export async function GET(request: NextRequest) {
 
       return Response.json(data, { status: 200 });
     } else {
-      return Response.json(
-        {
-          error: 'error',
-        },
-        { status: 400 }
-      );
+      return Response.json({
+        error: 'error'
+      }, { status: 400 });
     }
   } catch (error) {
     return Response.json(error, { status: 400 });
