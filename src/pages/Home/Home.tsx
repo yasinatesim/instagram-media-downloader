a57035ad2b8a796afe4e2c087cbe28602bb0d4cd
Yasin ATEŞ <yasinatesim@gmail.com>
2024-01-27 15:58:20 +0300

    fix: update api messages for user info api

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -53,6 +53,12 @@ const Home: React.FC = () => {
         headers: {
           Accept: 'application/json, text/plain, */*',
           'Content-Type': 'application/json',
+          'User-Agent':
+            'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
+          'X-IG-App-ID': '936619743392459',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
         },
         body: JSON.stringify({ username: username, token }),
       });
