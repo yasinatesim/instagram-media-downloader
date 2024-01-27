abf7f1d547f8d12c38c9740bd5a53143c7d43f7d
Yasin ATEŞ <yasinatesim@gmail.com>
2024-01-27 15:47:10 +0300

    fix: update api messages for user info api

--- src/app/api/get-user-info/route.ts
+++ src/app/api/get-user-info/route.ts
@@ -42,7 +42,8 @@ export async function POST(request: NextRequest) {
     if (recaptchaResponse.success && recaptchaResponse.score >= 0.5) {
       const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
       const headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
+        'User-Agent':
+          'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
         'X-IG-App-ID': '936619743392459',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
@@ -53,7 +54,7 @@ export async function POST(request: NextRequest) {
         headers,
       });
 
-//      return new Response(JSON.stringify({ error: await response.text() }), { status: 200 });
+      //      return new Response(JSON.stringify({ error: await response.text() }), { status: 200 });
       if (!response.ok) {
         return new Response(JSON.stringify({ error: response }), { status: 200 });
       }
