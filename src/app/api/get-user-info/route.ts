51c387fccbd2860508055e7fd70bbed99e726aa2
Yasin ATEŞ <yasinatesim@gmail.com>
2024-01-27 16:08:48 +0300

    fix: update api messages for user info api

--- src/app/api/get-user-info/route.ts
+++ src/app/api/get-user-info/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from 'next/server';
 
+import { REQUEST_HEADER } from '@/constants/requests';
+
 export const dynamic = 'force-dynamic';
 export const runtime = 'nodejs';
 export const fetchCache = 'default-no-store';
@@ -42,12 +44,7 @@ export async function POST(request: NextRequest) {
     if (recaptchaResponse.success && recaptchaResponse.score >= 0.5) {
       const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
       const headers = {
-        'User-Agent':
-          'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
-        'X-IG-App-ID': '936619743392459',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
+        ...REQUEST_HEADER,
       };
 
       const response = await fetch(url, {
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,6 +11,7 @@ import {
   INSTAGRAM_USERNAME_REGEX_FOR_PROFILE,
   INSTAGRAM_USERNAME_REGEX_FOR_STORIES,
 } from '@/constants/regexes';
+import { REQUEST_HEADER } from '@/constants/requests';
 import {
   INSTAGRAM_GRAPHQL_URL_FOR_HIGHLIGHTS,
   INSTAGRAM_GRAPHQL_URL_FOR_STORIES,
@@ -51,14 +52,9 @@ const Home: React.FC = () => {
       const response = await fetch('/api/get-user-info', {
         method: 'POST',
         headers: {
+          ...REQUEST_HEADER,
           Accept: 'application/json, text/plain, */*',
           'Content-Type': 'application/json',
-          'User-Agent':
-            'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
-          'X-IG-App-ID': '936619743392459',
-          'sec-fetch-dest': 'empty',
-          'sec-fetch-mode': 'cors',
-          'sec-fetch-site': 'same-origin',
         },
         body: JSON.stringify({ username: username, token }),
       });
