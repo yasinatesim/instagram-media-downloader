fb198456b73bba29f5f3728c186d653310b5d7a2
Yasin ATEŞ <yasinatesim@gmail.com>
2024-03-10 17:45:25 +0300

    fix: add new api service for profile picture

--- src/app/api/get-profile-picture/route.ts
+++ src/app/api/get-profile-picture/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest } from 'next/server';
 
-import { getProfilePictureUrlFromSaveIg } from '@/services/get-profile-picture-url';
 import { getUserInfo } from '@/services/get-user-info';
 import { loginFailedError } from '@/services/login-instagram';
 import verifyRecaptcha, { RECAPTCHA_THRESHOLD } from '@/services/verify-recaptcha';
@@ -21,38 +20,26 @@ export async function POST(request: NextRequest) {
       await sleep();
 
       try {
-        const data = await getProfilePictureUrlFromSaveIg(username);
+        const data = await getUserInfo(username);
 
-        return new Response(
-          JSON.stringify({
-            url: data,
-          }),
-          { status: 200 }
-        );
-      } catch (saveIgError) {
-        console.log('saveIgError API request failed. Trying alternative methods...', saveIgError);
-        try {
-          const data = await getUserInfo(username);
-
-          if (data?.hd_profile_pic_url_info?.url) {
-            return new Response(
-              JSON.stringify({
-                url: data.hd_profile_pic_url_info.url,
-              }),
-              { status: 200 }
-            );
-          } else {
-            await loginFailedError({ name: 'login_required', message: 'login_required' });
-            throw new Error('Session expired, please try again');
-          }
-        } catch (error) {
-          const errorMessage = error instanceof Error ? error.message : 'Something went wrong';
-          const errorResponse = { status: 'Failed', message: errorMessage };
-
-          await loginFailedError(error as Error);
-
-          return new Response(JSON.stringify({ error: errorResponse }), { status: 400 });
+        if (data?.hd_profile_pic_url_info?.url) {
+          return new Response(
+            JSON.stringify({
+              url: data.hd_profile_pic_url_info.url,
+            }),
+            { status: 200 }
+          );
+        } else {
+          await loginFailedError({ name: 'login_required', message: 'login_required' });
+          throw new Error('Session expired, please try again');
         }
+      } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'Something went wrong';
+        const errorResponse = { status: 'Failed', message: errorMessage };
+
+        await loginFailedError(error as Error);
+
+        return new Response(JSON.stringify({ error: errorResponse }), { status: 400 });
       }
     }
   } catch (error) {
--- src/services/get-user-info.ts
+++ src/services/get-user-info.ts
@@ -10,35 +10,52 @@ import generateDynamicHeaders from '@/utils/generateDynamicHeaders';
 export async function getUserInfo(username: string) {
   const userId = await getUserId(username);
   try {
-    const url = `https://i.instagram.com/api/v1/users/${userId}/info/`;
-    const headers = {
-      Accept: 'application/json, text/plain, */*',
-      'User-Agent':
-        'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-origin',
-      ...generateDynamicHeaders(),
-      cookie: await cookieString(),
-    };
-
-    const response = await axios.get(url, { headers, maxBodyLength: Infinity, maxRedirects: 0 });
+    const response = await axios.request({
+      method: 'GET',
+      url: `https://instagram-api-20231.p.rapidapi.com/api/get_user_info/${userId}`,
+      headers: {
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Host': 'instagram-api-20231.p.rapidapi.com',
+      },
+    });
 
     if (response.status !== 200) {
       throw new Error(`Request failed with status: ${response.status}`);
     }
 
-    const result = response.data.user;
+    return response.data.data;
+  } catch (rapidApiError) {
+    console.error('Instagram API request failed. Falling back to alternative method', rapidApiError);
 
-    return result;
-  } catch (error) {
-    console.error('Instagram API request failed. Falling back to alternative method');
     try {
-      const ig = await loginToInstagram();
-      return await ig.user.info(userId);
+      const url = `https://i.instagram.com/api/v1/users/${userId}/info/`;
+      const headers = {
+        Accept: 'application/json, text/plain, */*',
+        'User-Agent':
+          'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        ...generateDynamicHeaders(),
+        cookie: await cookieString(),
+      };
+
+      const response = await axios.get(url, { headers, maxBodyLength: Infinity, maxRedirects: 0 });
+
+      if (response.status !== 200) {
+        throw new Error(`Request failed with status: ${response.status}`);
+      }
+
+      return response.data.user;
     } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : 'Error retrieving Instagram user ID';
-      throw new Error(errorMessage);
+      console.error('Instagram API request failed. Falling back to alternative method');
+      try {
+        const ig = await loginToInstagram();
+        return await ig.user.info(userId);
+      } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'Error retrieving Instagram user ID';
+        throw new Error(errorMessage);
+      }
     }
   }
 }
