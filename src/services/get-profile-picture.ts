03c64516aeba7e9250c96a2cd3cac900eb811eb7
Yasin ATEŞ <yasinatesim@gmail.com>
2024-07-02 01:19:53 +0300

    feat: update profile picture doc id

--- src/services/get-profile-picture.ts
+++ src/services/get-profile-picture.ts
@@ -10,7 +10,7 @@ async function getProfilePictureFromInstagrmApi(username: string) {
   try {
     const response = await axios.post(
       'https://www.instagram.com/graphql/query',
-      `__d=www&__user=0&__a=1&__req=1o&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisUserHoverCardContentV2Query&variables=%7B%22userID%22%3A%22${userId}%22%2C%22username%22%3A%22${username}%22%7D&server_timestamps=true&doc_id=7666785636679494`,
+      `__d=www&__user=0&__a=1&__req=1o&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisUserHoverCardContentV2Query&variables=%7B%22userID%22%3A%22${userId}%22%2C%22username%22%3A%22${username}%22%7D&server_timestamps=true&doc_id=7953815318003879`,
       {
         headers: {
           accept: '*/*',
@@ -19,6 +19,7 @@ async function getProfilePictureFromInstagrmApi(username: string) {
           'content-type': 'application/x-www-form-urlencoded',
           dpr: '1',
           pragma: 'no-cache',
+          priority: 'u=1, i',
           'sec-ch-prefers-color-scheme': 'dark',
           'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
           'sec-ch-ua-full-version-list':
@@ -30,11 +31,11 @@ async function getProfilePictureFromInstagrmApi(username: string) {
           'sec-fetch-dest': 'empty',
           'sec-fetch-mode': 'cors',
           'sec-fetch-site': 'same-origin',
-          'viewport-width': '863',
+          'viewport-width': '1920',
           'x-asbd-id': '918374',
           'x-bloks-version-id': 'c6c7b90c445758d2f27d257e0b850f3b5e7b27dfbb81b6a470d6a40cb040a25a',
           'x-csrftoken': 'yNmAkP3tQl4eV7sHcDoRfW9gZsX1A2bE',
-          'x-fb-friendly-name': 'PolarisUserHoverCardContentV2Query',
+          'x-fb-friendly-name': 'PolarisUserHoverCardContentV2DirectQuery',
           'x-fb-lsd': 'pLxQbR4a9HgWu1sVyFnCdM3eLxP9sV7j',
           'x-ig-app-id': '672145893218637',
         },
