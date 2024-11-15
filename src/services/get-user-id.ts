f3721687d2b14755ce96249ac75252784ac7ec7b
Yasin ATEŞ <yasinatesim@gmail.com>
2024-11-15 08:14:01 +0300

    feat: update id url

--- src/services/get-user-id.ts
+++ src/services/get-user-id.ts
@@ -86,18 +86,22 @@ async function getUserIdFromProfilePage(username: string) {
   }
 }
 
-async function getUserIdFromSaveIg(username: string) {
+async function getUserIdFromSaveVid(username: string) {
   try {
-    const response = await axios.post('https://v3.saveig.app/api/get-url', `l=https://www.instagram.com/${username}/`, {
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'User-Agent':
-          'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-      },
-    });
+    const response = await axios.post(
+      'https://v3.savevid.net/api/get-url',
+      `l=https://www.instagram.com/${username}/`,
+      {
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'User-Agent':
+            'Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+        },
+      }
+    );
 
     if (response.status !== 200) {
       throw new Error('Network response was not ok');
@@ -123,7 +127,7 @@ async function getUserId(username: string) {
     console.log('profilePage request failed. Trying alternative methods...', profilePageError);
 
     try {
-      return await getUserIdFromSaveIg(username);
+      return await getUserIdFromSaveVid(username);
     } catch (saveIgError) {
       console.log('saveIgError API request failed. Trying alternative methods...', saveIgError);
       try {
